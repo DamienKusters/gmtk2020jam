1192501ab3bbecb00c417d9fe80d167af1f504a3
DamienKusters <damien.kusters@ziggo.nl>
2020-07-11 14:43:54 +0200

    Add dynamic rendering of DreamCards

--- Assets/Scripts/DreamCard.cs
+++ Assets/Scripts/DreamCard.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 public class DreamCard : MonoBehaviour
 {
     private Globals globals;
-    private Image imageTexture;
+    private Image image;
+    private Direction direction;
 
-    public Sprite[] DreamCardSprites;
+    public Sprite up, up_right, right, down_right, down, down_left, left, up_left, random;
 
 
     // Start is called before the first frame update
@@ -16,23 +17,56 @@ public class DreamCard : MonoBehaviour
     {
         globals = GameObject.Find("GLOBALS").GetComponent<Globals>();
 
-        imageTexture = gameObject.GetComponent<Image>();
-        imageTexture.sprite = DreamCardSprites[GenerateRandomDreamCard()];
+        image = gameObject.GetComponent<Image>();
+
+        GenerateRandomDreamCard();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void GenerateRandomDreamCard()
     {
-        
-    }
+        int rand = Random.Range(0, 9);
 
-    int GenerateRandomDreamCard()
-    {
-        return Random.Range(0, DreamCardSprites.Length);
+        switch (rand)
+        {
+            case 0:
+                image.sprite = up;
+                break;
+            case 1:
+                image.sprite = up_right;
+                break;
+            case 2:
+                image.sprite = right;
+                break;
+            case 3:
+                image.sprite = down_right;
+                break;
+            case 4:
+                image.sprite = down;
+                break;
+            case 5:
+                image.sprite = down_left;
+                break;
+            case 6:
+                image.sprite = left;
+                break;
+            case 7:
+                image.sprite = up_left;
+                break;
+            case 8:
+                image.sprite = random;
+                break;
+        }
+
+        direction = (Direction)rand;
     }
 
     public void onClick()
     {
-        Debug.Log("Clicked");
+        globals.direction = direction;
+        globals.directionChanged = true;
+
+        Debug.Log(direction.ToString());
     }
+
+    
 }
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Globals : MonoBehaviour
 {
-    public string direction = "up";
+    public bool directionChanged = false;//The sandman script will get this value: true = grab the direction specified in globals, false = generate random
+    public Direction direction = Direction.RIGHT;
+}
+public enum Direction
+{
+    UP,
+    UP_RIGHT,
+    RIGHT,
+    DOWN_RIGHT,
+    DOWN,
+    DOWN_LEFT,
+    LEFT,
+    UP_LEFT,
+    RANDOM
 }
