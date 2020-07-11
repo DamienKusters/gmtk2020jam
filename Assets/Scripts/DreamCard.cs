18a22ba33dde27b2e341561f0fedb344c1e92fc5
DamienKusters <damien.kusters@ziggo.nl>
2020-07-11 11:53:39 +0200

    Add DreamCard.cs

---
+++ Assets/Scripts/DreamCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DreamCard : MonoBehaviour
+{
+    public Sprite up_left, up, up_right, right, down_right, down, down_left, left, shuffle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
