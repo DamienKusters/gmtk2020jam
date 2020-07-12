f5eb4f1348c5da02ef80163ddf8dc4a63261511c
DamienKusters <damien.kusters@ziggo.nl>
2020-07-12 11:26:31 +0200

    Update menu screen

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClick()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+    public void onClick_close()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
